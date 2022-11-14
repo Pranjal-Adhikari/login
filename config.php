79ab04f7692f466ab2aaf43ff52805f64afc2af2
Mr Joe <99041614+Pranjal-Adhikari@users.noreply.github.com>
2022-11-14 17:17:07 -0600

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-   define('DB_SERVER', `${{ secrets.HOST }}`);
+   define('DB_SERVER', `${{ secrets.MYSQL_SERVER }}`);
    define('DB_USERNAME', 'root');
    define('DB_PASSWORD', 'rootpassword');
    define('DB_DATABASE', 'database');
