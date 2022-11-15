ffb8b8d56562703f20c6179dea2a46a47e075974
Mr Joe <99041614+Pranjal-Adhikari@users.noreply.github.com>
2022-11-14 18:03:13 -0600

    Create dbh.inc.php

---
+++ dbh.inc.php
@@ -0,0 +1,14 @@
+//dbh.inc.php
+< ?php
+ 
+$serverName = "localhost";
+$dBUsername = "root";    //Database username
+$dBPassword = "root";    //Database password
+$dBName = "loginsystem"; //Database servername
+ 
+$conn = mysqli_connect($serverName, $dBUsername, $dBPassword, $dBName, 3306);
+ 
+if (!$conn)
+{
+    die("Connection failed: ". mysqli_connect_error());
+}
