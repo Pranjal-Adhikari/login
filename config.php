67062dff4093352c89a14cf36be49154edc1580f
Mr Joe <99041614+Pranjal-Adhikari@users.noreply.github.com>
2022-11-14 18:02:47 -0600

    Delete config.php

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php
-   define('DB_SERVER', `${{ secrets.MYSQL_SERVER }}`);
-   define('DB_USERNAME', 'root');
-   define('DB_PASSWORD', 'rootpassword');
-   define('DB_DATABASE', 'database');
-   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
-?>
